a38e9948d3321902527ad21af222bab720cf1fd8
gulcantpl <gulcan.tpl9049@gmail.com>
2025-07-31 13:33:19 +0300

    click sound added

--- src/script.js
+++ src/script.js
@@ -5,21 +5,38 @@ const decrementBtn = document.getElementById('decrement');
 const resetBtn = document.getElementById('reset');
 const doubleBtn = document.getElementById('double');
 
+const clickSound = new Audio('sound/click.wav');
+Array.from(document.getElementsByClassName('btn-sound')).forEach((button) => {
+  button.addEventListener('click', () => {
+    clickSound.currentTime = 0;
+    clickSound.play();
+  });
+});
+
 incrementBtn.addEventListener('click', () => {
+  clickSound.currentTime = 0;
+  clickSound.play();
   count++;
   counter.textContent = count;
 });
+
 decrementBtn.addEventListener('click', () => {
+  clickSound.currentTime = 0;
+  clickSound.play();
   count--;
   counter.textContent = count;
 });
 
 resetBtn.addEventListener('click', () => {
+  clickSound.currentTime = 0;
+  clickSound.play();
   count = 0;
   counter.textContent = count;
 });
 
 doubleBtn.addEventListener('click', () => {
+  clickSound.currentTime = 0;
+  clickSound.play();
   count *= 2;
   counter.textContent = count;
 });
